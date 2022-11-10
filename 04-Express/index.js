5ea014ccc7aac27f37a5d256372b0d0f1628e323
Kenny <kennylima@hotmail.com>
2022-11-09 21:15:55 -0300

    Criando um direcionamento do node para uma página HTML

--- 04-Express/index.js
+++ 04-Express/index.js
@@ -1,9 +1,11 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const porta = 3000 //Porta para acessar o servidor
+const basePath = path.join(__dirname, `Templates`)
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Essa é a minha primeira página utilizando o nodeJs")
+    resposta.sendFile(`${basePath}/index.html`)
 })
 
 app.listen(porta, ()=>{
